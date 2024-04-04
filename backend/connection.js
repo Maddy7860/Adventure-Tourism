bbc7b50f9258f97a3fd06d01a2d981b02531421b
Maddy7860 <techmaddy786@gmail.com>
2024-04-04 18:09:10 +0530

    commit

--- backend/connection.js
+++ backend/connection.js
@@ -4,3 +4,13 @@ const url="mongodb+srv://techmaddy786:Maddy786@mirza786.iaccnfc.mongodb.net/myne
 
 // asynchronous functions - return Promise
 mongoose.connect(url)
+.then((result) => {
+    console.log('database connected');
+    
+}).catch((err) => {
+    console.log('err');
+    
+});
+
+console.log('last line');
+    
